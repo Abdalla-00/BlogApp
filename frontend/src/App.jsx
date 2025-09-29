1b5d872884b9b10d9ace807ed3902aee02f30f36
Abdalla <anddragon34@gmail.com>
2025-09-29 20:56:26 +0300

    add reac-router-dom and create login/registarPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import { Button } from "./components/ui/button";
+import { Outlet } from "react-router-dom";
+
 
 
 function App() {
   return (
     <>
       <h1 className="bg-amber-400 text-white text-center p-5">hello am apdala</h1>
-      <Button>Button</Button>
+   
+      <Outlet/>
     </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RegistarPage from "./page/registarPage";
+import LoginPage from "./page/LoginPage";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/registar",
+        element: <RegistarPage />,
+      },
+      {
+        path: "/LoginPage",
+        element: <LoginPage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
