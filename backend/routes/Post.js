5591a9f6a99a0fd4b5d4cad3a26a803b613a6fc5
Abdalla <anddragon34@gmail.com>
2025-12-23 00:55:02 +0300

    add multar

--- backend/routes/Post.js
+++ backend/routes/Post.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createPost } from "../controller/postController.js";
 import { authenticate } from "../middleware/authmiddleware.js";
+import upload from "../middleware/uploadCloudniray.js";
 
 const postRoute = express.Router();
 
-postRoute.post("/create_post",authenticate , createPost);
+postRoute.post("/create_post",authenticate , upload , createPost);
 
 export default postRoute;
