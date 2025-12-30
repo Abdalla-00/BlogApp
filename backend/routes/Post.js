6b62fbe74f55b23f7c21ce98b2a8a6c4e9120c54
Abdalla <anddragon34@gmail.com>
2025-12-30 23:26:25 +0300

    add delete route

--- backend/routes/Post.js
+++ backend/routes/Post.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createPost } from "../controller/postController.js";
+import { createPost, deletePost } from "../controller/postController.js";
 import { authenticate } from "../middleware/authmiddleware.js";
 import upload from "../middleware/uploadCloudniray.js";
 
 const postRoute = express.Router();
 
 postRoute.post("/create_post",authenticate , upload , createPost);
+
+postRoute.post("/delete_post",authenticate , deletePost);
 
 export default postRoute;
